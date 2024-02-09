e929571cc2eb8bf71833f67176de3f87e638ac50
Marina Yeremina <marina.era121@mail.ru>
2024-02-09 21:41:26 +0500

    add cproj3

--- Program.cs
+++ Program.cs
@@ -27,27 +27,48 @@
 
 // Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
-Console.Write("Введите значение m: ");
-int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение m: ");
+// int m = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите значение n: ");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-int result = FindAckerman(m, n);
+// int result = FindAckerman(m, n);
 
-Console.WriteLine($"A({m}, {n}) = {result}");
+// Console.WriteLine($"A({m}, {n}) = {result}");
 
-Console.ReadLine(); 
+// Console.ReadLine(); 
 
-int FindAckerman(int m, int n)
+// int FindAckerman(int m, int n)
+// {
+//     if (m == 0)
+//         return n + 1;
+//     else if (n == 0)
+//         return FindAckerman(m - 1, 1);
+//     else
+//         return FindAckerman(m - 1, FindAckerman(m, n - 1));
+// }
+
+// Задача 3: Задайте произвольный массив. Выведите его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+int[] array = { 1, 2, 5, 0, 10, 34 };
+
+Console.WriteLine("Исходный массив:");
+PrintArrayReversed(array, array.Length - 1);
+
+Console.ReadLine();
+
+
+void PrintArrayReversed(int[] arr, int index)
 {
-    if (m == 0)
-        return n + 1;
-    else if (n == 0)
-        return FindAckerman(m - 1, 1);
-    else
-        return FindAckerman(m - 1, FindAckerman(m, n - 1));
+    if (index < 0)
+        return;
+
+    Console.Write(arr[index] + " ");
+
+    PrintArrayReversed(arr, index - 1);
 }
+
 
 
 
